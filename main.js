68659d4a804d671b859561b7e8b9b6beea6a8b53
Jeongsunga <jsa3341@gmail.com>
2024-07-10 21:52:12 +0900

    make filter

--- main.js
+++ main.js
@@ -1,10 +1,33 @@
 let newsList = []
+const menus = document.querySelectorAll(".menus button")
+menus.forEach(menu => menu.addEventListener("click", (event)=>getNewsByCategory(event)))
+
 const getLatestNews = async () => {
     const url = new URL(`https://newspage100.netlify.app/top-headlines?country=kr`)
     const response = await fetch(url)
     const data = await response.json()
     newsList = data.articles
     console.log(newsList)
+    render()
+}
+
+const getNewsByCategory = async (event) => {
+    const category = event.target.textContent.toLowerCase()
+    console.log("category", category)
+    const url = new URL(`https://newspage100.netlify.app/top-headlines?country=kr&category=${category}`)
+    const response = await fetch(url)
+    const data = await response.json()
+    newsList = data.articles
+    render()
+}
+
+const searchNews = async() => {
+    const keyword = document.getElementById("search-input").value
+    console.log("keyword", keyword)
+    const url = new URL(`https://newspage100.netlify.app/top-headlines?country=kr&q=${keyword}`)
+    const response = await fetch(url)
+    const data = await response.json()
+    newsList = data.articles
     render()
 }
 
@@ -50,3 +73,7 @@ const openSearchBox = () => {
 };
 
 getLatestNews()
+
+// 1. 버튼에 클릭 이벤트 주기
+// 2. 카테고리별 뉴스 가져오기
+// 3. 뉴스 보여주기
